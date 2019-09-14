20726b6d7b84484cd46541ab15a9006160cb3868
Carolina Boschetti <30893634+carolinaboschetti@users.noreply.github.com>
2019-09-14 16:33:28 +0200

    :sparkles:Add main game functions

---
+++ script.js
@@ -0,0 +1,25 @@
+    const CHOICES = ["rock", "paper", "scissors"]
+    let computerChoice
+    let userChoice
+    let isUserWinner
+
+    const submit = function() {
+        computerChoice = CHOICES[Math.floor(Math.random() * CHOICES.length)]
+        userChoice = document.querySelector(".radio:checked").value
+       checkWinner()
+       console.log(userChoice, computerChoice, isUserWinner)
+   }
+   const checkWinner = () => {
+       if (userChoice === "rock" && computerChoice === "scissors") {
+           isUserWinner = true
+       } else if (userChoice === "paper" && computerChoice === "rock") {
+           isUserWinner === true
+       } else if (userChoice === "scissors" && computerChoice === "paper"){
+           isUserWinner === true
+       } else if (userChoice === computerChoice) {
+           isUserWinner = false
+       } else {
+           isUserWinner = false
+       }
+       return isUserWinner
+   }
